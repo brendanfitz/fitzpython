a41b956da320606e25634107d0e35132784c023b
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-08-25 13:50:58 -0400

    Add docstring to pandas_util

--- fitzpython/pandas_utils.py
+++ fitzpython/pandas_utils.py
@@ -1,6 +1,15 @@
 # pandas_utils.py
 
 def format_cols(colname, direction='in'):
+    """Formats columns beween human-readable and pandorable
+
+    Keyword arguments:
+    real -- the real part (default 0.0)
+    imag -- the imaginary part (default 0.0)
+    """
+    if imag == 0.0 and real == 0.0:
+        return complex_zero
+    ...
     if direction == 'in':
         return (colname
             .lower()
