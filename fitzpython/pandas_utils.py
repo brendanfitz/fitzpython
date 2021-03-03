4b31633a1db11436461c5a8fb830eecafd8232d8
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2021-03-03 15:50:39 -0500

    Create autofit_column_widths function

--- fitzpython/pandas_utils.py
+++ fitzpython/pandas_utils.py
@@ -22,3 +22,19 @@ def format_cols(colname, direction='in'):
             .title()
         )
     raise ValueError('Direction must be "in" or "out"')
+    
+    
+from openpyxl.utils import get_column_letter
+
+def autofit_column_widths(ws):
+    for col in ws.columns:
+         max_length = 0
+         column = col[0].column
+         for cell in col:
+             try: # Necessary to avoid error on empty cells
+                 if len(str(cell.value)) > max_length:
+                     max_length = len(cell.value)
+             except:
+                 pass
+         adjusted_width = (max_length + 2) * 1.2
+         ws.column_dimensions[get_column_letter(column)].width = adjusted_width
